3bfe51b508498a5651efc789290074986d161a45
rohitdhimal <rohit.dhimal19@gmail.com>
2022-12-01 15:55:14 -0500

    user profile and modules and quiz page added

---
+++ frontend-react/src/Pages/UserProfile/UserProfile.js
@@ -0,0 +1,23 @@
+import { Fragment } from "react";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
+import Header from '../../Components/Header/Header';
+import Footer from '../../Components/Footer/Footer';
+
+import "./UserProfile.css";
+
+
+const UserProfile = () => {
+  return (
+    <Fragment>
+        <Container>
+            <Row>
+                <Col>
+                </Col>
+            </Row>
+        </Container>
+    </Fragment>
+
+  );
+};
+
+export default UserProfile;
