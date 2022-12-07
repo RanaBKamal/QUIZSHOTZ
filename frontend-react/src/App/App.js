799aecf9d8f5b6feb989f1942de31e1f4dbf1ac5
Kamal B. Rana <magarkamalrb@gmail.com>
2022-12-07 10:24:50 -0500

    admin pages for frontend

--- frontend-react/src/App/App.js
+++ frontend-react/src/App/App.js
@@ -7,6 +7,10 @@ import SignUp from '../Pages/SignUp/SignUp';
 import Login from '../Pages/Login/Login';
 import UserProfile from "../Pages/UserProfile/UserProfile";
 
+import AdminDashboard from '../Admin/Pages/AdminDashboard';
+import AddModule from '../Admin/Pages/AddModule';
+import AddQuestion from '../Admin/Pages/AddQuestion';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -20,6 +24,10 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/userprofile" element={<UserProfile />} />
+        <Route path="/admin" element={<AdminDashboard/>}>
+          <Route path="add-module" element={<AddModule/>}/>
+          <Route path="add-question" element={<AddQuestion/>}/>
+        </Route>
       </Routes>
     </Router>
   );
