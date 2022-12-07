3f24178c06f99b298fa1cfddd976804950e9ab9d
Chiranjibi Karki <ckarki70@gmail.com>
2022-12-07 13:44:49 -0500

    login functionality added

--- frontend-react/src/Pages/Login/Login.js
+++ frontend-react/src/Pages/Login/Login.js
@@ -1,11 +1,35 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 import './Login.css';
 import './Login.css';
+import axios from "axios";
 
 const Login = () => {
+
+    const[user, setUser]= useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange=(e)=>{
+        setUser({...user, [e.target.name]:e.target.value})
+    };
+
+    const handleLogin=async(e)=>{
+        e.preventDefault();
+        try {
+            await axios.post(process.env.REACT_APP_SIGNIN_API,user,{
+                headers: {
+                    'x-access-token':''
+                }
+            })
+        } catch (error) {          
+        }
+    }
+
+
     return (
         <Fragment>
             <Header/>
@@ -26,7 +50,7 @@ const Login = () => {
                             <p className='fs-2 fw-bold'> <span className='login-rect'>. </span> Login Form</p>
                             <Form.Group className="mb-3" controlId="formBasicEmail">
                                 <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Control type="email" placeholder="Enter email" name="email" value={user.email} onChange={handleChange}/>
                                 <Form.Text className="text-muted">
                                 We'll never share your email with anyone else.
                                 </Form.Text>
@@ -34,12 +58,12 @@ const Login = () => {
 
                             <Form.Group className="mb-3" controlId="formBasicPassword">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
+                                <Form.Control type="password" placeholder="Password" name="password" value={user.password} onChange={handleChange}/>
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="formBasicCheckbox">
                                 <Form.Check type="checkbox" label="Remember me" />
                             </Form.Group>
-                            <Button className='btn btn-lg bg-orange w-100' type="submit">
+                            <Button className='btn btn-lg bg-orange w-100' type="submit" onClick={handleLogin}>
                                 Submit
                             </Button>
                         </Form>
